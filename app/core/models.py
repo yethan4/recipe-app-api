764ec9ec7c550fd57c91a39c8675946a2c10671c
yethan <yethan444@gmail.com>
2025-05-22 11:20:55 +0200

    Clean up code to comply with flake8

--- app/core/models.py
+++ app/core/models.py
@@ -1,16 +1,17 @@
-""" 
+"""
 Database models.
 """
 from django.db import models
 from django.contrib.auth.models import (
-	AbstractBaseUser,
-	BaseUserManager,
-	PermissionsMixin,
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
 )
+
 
 class UserManager(BaseUserManager):
     """Manager for users"""
-    
+
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user"""
         if not email:
@@ -18,16 +19,16 @@ class UserManager(BaseUserManager):
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        
+
         return user
-    
+
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
-        
+
         return user
 
 
@@ -37,7 +38,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
+
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,21 +1,22 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models."""
-    
+
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-			email=email,
-			password=password
-		)
-        
+            email=email,
+            password=password
+        )
+
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-        
+
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users"""
         sample_emails = [
@@ -39,6 +40,6 @@ class ModelTests(TestCase):
             'test@example.com',
             'test123'
         )
-        
+
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
